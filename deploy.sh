037895acadd999b3e80a0ef1753b5bfd4625dcc2
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2022-12-16 06:57:36 -0400

    oficial
    oficial

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,24 @@ sudo ufw status
 sudo ufw enable
 sudo ufw app list
 sudo ufw allow 'OpenSSH'
+sudo ufw allow 80
 sudo ufw allow 5432
+sudo ufw allow 2222
+
+sudo ufw allow 10001
+sudo ufw allow 20001
+sudo ufw allow 30001
+
+sudo ufw allow 10050 
+sudo ufw allow 20050 
+sudo ufw allow 30050 
+
+sudo ufw allow 10051 
+sudo ufw allow 20052 
+sudo ufw allow 30053 
 sudo ufw status
 
 sudo docker-compose down
 sudo docker-compose build
 sudo docker-compose up
+# dejarlo en background sudo docker-compose up -d
