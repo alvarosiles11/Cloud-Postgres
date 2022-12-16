58f495597d4f0886c85c60c0a8b28c71cb72034a
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2022-12-15 23:36:01 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,19 @@
 # 1ro chmod u+r+x deploy.sh
 # 2do ./deploy.sh
 
+sudo apt-get update
+sudo apt install docker.io
+sudo apt  install docker-compose
+sudo docker -v
+sudo docker-compose -v
+
+sudo ufw status
+sudo ufw enable
+sudo ufw app list
+sudo ufw allow 'OpenSSH'
+sudo ufw allow 5432
+sudo ufw status
+
 docker-compose down
 docker-compose build
 docker-compose up -d
